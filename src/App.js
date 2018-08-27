228986974cd9532d73dc6eb75731d375cec1eaca
Lisa Ross <lr@footprintleadership.com>
2018-08-27 11:57:53 -0400

    :sparkles: onclick adopt me event
    function for handleTitleClick in react component #2

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ])
+const Pet = props => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, props.name),
+    React.createElement('h2', {}, props.animal),
+    React.createElement('h2', {}, props.breed)
+  ]);
+};
+class App extends React.Component {
+  handleTitleClick() {
+    alert('you clicked the title');
+  }
+  render() {
+    return React.createElement(
+      'div',
+      {},
+      React.createElement('h1', { onClick: this.handleTitleClick }, 'Adopt me!'),
+      React.createElement(Pet, {
+        name: 'Moxie',
+        animal: 'cat',
+        breed: 'Tuxedo'
+      }),
+      React.createElement(Pet, {
+        name: 'Jack',
+        animal: 'cat',
+        breed: 'Tiger'
+      }),
+      React.createElement(Pet, {
+        name: 'Luna',
+        animal: 'dog',
+        breed: 'Havanese'
+      })
+    );
+  }
 }
 
-const App = () => {
-  return React.createElement("div", {},
-    React.createElement('h1', {}, 'Adopt me!'),
-    React.createElement(Pet, {
-      name: "Moxie",
-      animal: "cat",
-      breed: "Tuxedo"
-    }),
-    React.createElement(Pet, {
-      name: "Jack",
-      animal: "cat",
-      breed: "Tiger"
-    }),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "dog",
-      breed: "Havanese"
-    })
-  )
-}
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
