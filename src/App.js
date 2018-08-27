348d355d53d76db147c994b814808566ab7a4f4b
Lisa Ross <lr@footprintleadership.com>
2018-08-27 12:16:54 -0400

    :art: linting files with prettier defaults
    prettier defaults #1

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
 const Pet = props => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, props.name),
-    React.createElement('h2', {}, props.animal),
-    React.createElement('h2', {}, props.breed)
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
   ]);
 };
 class App extends React.Component {
   handleTitleClick() {
-    alert('you clicked the title');
+    alert("you clicked the title");
   }
   render() {
     return React.createElement(
-      'div',
+      "div",
       {},
-      React.createElement('h1', { onClick: this.handleTitleClick }, 'Adopt me!'),
+      React.createElement(
+        "h1",
+        { onClick: this.handleTitleClick },
+        "Adopt me!"
+      ),
       React.createElement(Pet, {
-        name: 'Moxie',
-        animal: 'cat',
-        breed: 'Tuxedo'
+        name: "Moxie",
+        animal: "cat",
+        breed: "Tuxedo"
       }),
       React.createElement(Pet, {
-        name: 'Jack',
-        animal: 'cat',
-        breed: 'Tiger'
+        name: "Jack",
+        animal: "cat",
+        breed: "Tiger"
       }),
       React.createElement(Pet, {
-        name: 'Luna',
-        animal: 'dog',
-        breed: 'Havanese'
+        name: "Luna",
+        animal: "dog",
+        breed: "Havanese"
       })
     );
   }
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
