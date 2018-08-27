f7e099828f668ecbb406d6f3d38419ff5d0f0d98
Lisa Ross <lr@footprintleadership.com>
2018-08-27 15:57:38 -0400

    :sparkles: pull from api
    animals from seattle #6

--- src/App.js
+++ src/App.js
@@ -1,15 +1,68 @@
 import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
+import pf from "petfinder-client";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // define default initial state
+      pets: [] // empty array
+    };
+  }
+  componentDidMount() {
+    petfinder.pet
+      .find({ output: "full", location: "Seattle, WA" })
+      .then(data => {
+        let pets;
+
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        this.setState({
+          pets
+        });
+      });
+  }
   render() {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <Pet name="Luna" animal="dog" breed="Havanese" />
-        <Pet name="Moxy" animal="cat" breed="Tuxedo" />
-        <Pet name="Jack" animal="cat" breed="Tiger" />
+        <div>
+          {/* map array of pets to an array of components */}
+          {this.state.pets.map(pet => {
+            let breed;
+            {
+              /* some animals have multiple breeds - join them into one string here */
+            }
+            if (Array.isArray(pet.breeds.breed)) {
+              breed = pet.breeds.breed.join(", ");
+            } else {
+              breed = pet.breeds.breed;
+            }
+            return (
+              <Pet
+                key={pet.id}
+                animal={pet.animal}
+                name={pet.name}
+                breed={breed}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
