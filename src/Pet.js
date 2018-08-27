117cab1353edf75ad030ea40c3a3f5789c11fd5a
Lisa Ross <lr@footprintleadership.com>
2018-08-27 16:08:48 -0400

    :art: data from API
    class for pet and import props #6

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,31 @@
-import React from "react";
-const Pet = props => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
-  );
-};
+import React, { Component } from "react";
+
+class Pet extends Component {
+  render() {
+    // destructure
+    const { name, animal, breed, media, location } = this.props;
+
+    let photos = [];
+
+    if (media && media.photos && media.photos.photo) {
+      // array ONLY of photos with only sized pn
+      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+    }
+
+    return (
+      <div className="pet">
+        <div className="image-container">
+          <img src={photos[0].value} alt={name} />
+        </div>
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+        </div>
+      </div>
+    );
+  }
+}
+
 export default Pet;
