ab8ac8ba7f2f8c07b34606b758b0e8a74bda65e2
Lisa Ross <lr@footprintleadership.com>
2018-08-27 14:45:10 -0400

    :wheelchair: remove onclick h1
    remove event from h1 and handler #5

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 class App extends React.Component {
-  handleTitleClick() {
-    alert("you clicked the title");
-  }
   render() {
     return (
       <div>
-        <h1 onClick={this.handleTitleClick}>Adopt Me!</h1>
+        <h1>Adopt Me!</h1>
         <Pet name="Luna" animal="dog" breed="Havanese" />
         <Pet name="Moxy" animal="cat" breed="Tuxedo" />
         <Pet name="Jack" animal="cat" breed="Tiger" />
