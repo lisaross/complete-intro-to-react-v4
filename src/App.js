7c17b77c31947d7eb6bd09fa254b6c57bcf0bd05
Lisa Ross <lr@footprintleadership.com>
2018-08-27 12:55:55 -0400

    :heavy_plus_sign: react imports
    react imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-/* global React ReactDOM */
+import React from "react";
+import { render } from "react-dom";
 
 const Pet = props => {
   return React.createElement("div", {}, [
@@ -39,4 +40,4 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
