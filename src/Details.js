7bc4faee4ad9227bf20d2e3d4d674a5fd6019d86
Lisa Ross <lr@footprintleadership.com>
2018-08-28 11:25:00 -0400

    :art: fix image issue
    fix image thing - TODO go back and review

--- src/Details.js
+++ src/Details.js
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import pf from "petfinder-client";
-import { navigate } from "@reach/router/lib/history";
-import { Carousel } from "./Carousel";
+import Carousel from "./Carousel";
 
 const petfinder = pf({
   key: process.env.API_KEY,
   secret: process.env.API_SECRET
 });
 
-class Details extends Component {
-  state = {
-    loading: true
-  };
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = { loading: true };
+  }
   componentDidMount() {
     petfinder.pet
       .get({
@@ -20,46 +20,44 @@ class Details extends Component {
         id: this.props.id
       })
       .then(data => {
-        const pet = data.petfinder.pet;
         let breed;
-        if (Array.isArray(pet.breeds.breed)) {
-          breed = pet.breeds.breed.join(", ");
+        if (Array.isArray(data.petfinder.pet.breeds.breed)) {
+          breed = data.petfinder.pet.breeds.breed.join(", ");
         } else {
-          breed = pet.breeds.breed;
+          breed = data.petfinder.pet.breeds.breed;
         }
-
         this.setState({
-          name: pet.name,
-          animal: pet.animal,
-          location: `${pet.contact.city}, ${pet.contact.state}`,
-          description: pet.description,
-          media: pet.media,
+          name: data.petfinder.pet.name,
+          animal: data.petfinder.pet.animal,
+          location: `${data.petfinder.pet.contact.city}, ${
+            data.petfinder.pet.contact.state
+          }`,
+          description: data.petfinder.pet.description,
+          media: data.petfinder.pet.media,
           breed,
           loading: false
         });
       })
-      .catch(() => {
-        navigate("/");
-      });
+      .catch(err => this.setState({ error: err }));
   }
   render() {
     if (this.state.loading) {
-      return <h1>loading...</h1>;
+      return <h1>loading … </h1>;
     }
-    const { name, animal, breed, location, description, media } = this.state;
+
+    const { animal, breed, location, description, media } = this.state;
 
     return (
       <div className="details">
         <Carousel media={media} />
         <div>
           <h1>{name}</h1>
-          <h2>
-            {animal} - {breed} - {location}
-          </h2>
+          <h2>{`${animal} — ${breed} — ${location}`}</h2>
           <p>{description}</p>
         </div>
       </div>
     );
   }
 }
+
 export default Details;
